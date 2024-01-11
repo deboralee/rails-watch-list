2fe0cfbcb3c3a7dff34c9aed2c05ac6126c8a003
debora lee <debora.leesilva@gmail.com>
2024-01-10 23:09:56 -0300

    created view of bookmarks and updated views of lists action index and show

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check do
   resources :lists, only: [:index, :show, :new, :create] do
-    member do
-      
-    end
+    resources :bookmarks, only: [:new, :create]
   end
-
+  delete "bookmarks/:id", to: "bookmarks#destroy"
   # Defines the root path route ("/")
   # root "posts#index"
 end
